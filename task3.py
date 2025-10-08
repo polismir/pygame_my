4733e031f1248c829aa1bbd14f79f26bd7784097
polismir <pollismirnova221211@gmail.com>
2025-10-08 19:42:25 +0400

    Create task3.py

---
+++ task3.py
@@ -0,0 +1,47 @@
+import pygame as pg
+
+FPS = 60
+WIDTH, HEIGHT = 600, 500
+MINT = (230, 254, 212)
+ORANGE = (255, 150, 100)
+
+pg.init()
+screen = pg.display.set_mode((WIDTH, HEIGHT))
+pg.display.set_caption("Игра")
+clock = pg.time.Clock()
+
+# до начала игрового цикла отображаем объекты:
+# координаты центра круга
+x, y = WIDTH / 2, HEIGHT / 2  # координаты центра круга
+r = 30  # радиус круга
+pg.draw.circle(screen, ORANGE, (x, y), r)  # рисуем круг
+pg.display.update()  # обновляем окно
+
+flag_play = True
+while flag_play:
+    clock.tick(FPS)
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            flag_play = False
+            break
+    if not flag_play:
+        break
+
+    # изменение состояний объектов:
+    keys = pg.key.get_pressed()
+    if keys[pg.K_LEFT] and x - r > 0:
+        x -= 3
+    elif keys[pg.K_RIGHT] and x < WIDTH - r:
+        x += 3
+    elif keys[pg.K_UP] and y - r > 0:
+        y -= 3
+    elif keys[pg.K_DOWN] and y < HEIGHT - r:
+        y += 3
+
+
+    screen.fill(MINT)  # заливаем фон, стирая предыдущий круг
+    pg.draw.circle(screen, ORANGE, (x, y), r)  # рисуем новый, сдвинутый круг
+
+    pg.display.update()  # обновляем окно
