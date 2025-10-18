23b081020abd9248f9186bd2df18ce5b4af31573
polismir <pollismirnova221211@gmail.com>
2025-10-18 19:09:08 +0400

    task4.py

--- task4.py
+++ task4.py
@@ -18,23 +18,39 @@ x, y = random.randint(r, WIDTH - r), random.randint(r, HEIGHT - r)
 pg.draw.circle(screen, ORANGE, (x, y), r)  # рисуем круг
 pg.display.update()  # обновляем окно
 
-flag_play = True
-while flag_play:
+while 1:
     clock.tick(FPS)
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
             pg.quit()
-            flag_play = False
-            break
-    if not flag_play:
-        break
+        elif event.type == pg.MOUSEBUTTONDOWN and event.button == 4:
+            pos = pg.mouse.get_pos()
+            dist = ((pos[0] - x) ** 2 + (pos[1] - y) ** 2) ** 0.5
+            if dist <= r and r <= 150:
+                r += 2
+        elif event.type == pg.MOUSEBUTTONDOWN and event.button == 5:
+            pos = pg.mouse.get_pos()
+            dist = ((pos[0] - x) ** 2 + (pos[1] - y) ** 2) ** 0.5
+            if dist <= r and r >= 10:
+                r -= 2
 
     pressed = pg.mouse.get_pressed()
     if pressed[1]:
         pos = pg.mouse.get_pos()
-    dist = ((pos[0]-x)**2 + (pos[1]-y)**2) ** 0.5
-    if r <= dist :
+        dist = ((pos[0]-x)**2 + (pos[1]-y)**2) ** 0.5
+        if dist <= r:
+            x, y = random.randint(r, WIDTH - r), random.randint(r, HEIGHT - r)
+    if pressed[0]:
+        pos = pg.mouse.get_pos()
+        dist = ((pos[0] - x) ** 2 + (pos[1] - y) ** 2) ** 0.5
+        if dist <= r and r <= 150:
+            r += 1
+    if pressed[2]:
+        pos = pg.mouse.get_pos()
+        dist = ((pos[0] - x) ** 2 + (pos[1] - y) ** 2) ** 0.5
+        if dist <= r and r >= 10:
+            r -= 1
 
 
     screen.fill(MINT) # заливаем фон, стирая предыдущий круг
