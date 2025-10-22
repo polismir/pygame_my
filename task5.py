938a697dca6916b5994afebb03d457efc3fbb530
polismir <pollismirnova221211@gmail.com>
2025-10-22 19:41:13 +0400

    Update task5.py

--- task5.py
+++ task5.py
@@ -4,9 +4,11 @@ import pygame as pg
 # здесь определяются константы, функции и классы:
 FPS = 60
 WIDTH, HEIGHT = 1000, 600
-BALL_WIDTH, BALL_HEIGHT = WIDTH * 1 / 4, HEIGHT * 1 / 4
+BALL_WIDTH, BALL_HEIGHT = WIDTH * 1 / 8, HEIGHT * 1 / 4
 BROWN = (66, 53, 51)
 GREY = (184, 166, 163)
+BLUE = (79, 120, 171)
+RED = (130, 39, 30)
 
 # здесь происходит инициализация:
 pg.init()
@@ -22,11 +24,19 @@ pg.draw.rect(background, GREY, (0, 350, WIDTH, 120))
 
 ball = pg.Surface((BALL_WIDTH, BALL_HEIGHT), pg.SRCALPHA)
 ball.fill((0, 0, 0, 0))
-pg.draw.circle(ball, )
+pg.draw.circle(ball, (*BLUE, 100), (BALL_WIDTH / 2, BALL_HEIGHT / 2), 40)
+x, y = 0, 135
+
+# ball1 = pg.Surface((BALL_WIDTH, BALL_HEIGHT), pg.SRCALPHA)
+# ball1.fill((0, 0, 0, 0))
+# pg.draw.circle(ball1, (*RED, 100), (BALL_WIDTH / 2, BALL_HEIGHT / 2), 40)
+# x1, y1 = 880, 335
 
 # если надо до игрового цикла (=на самом старте игры) отобразить объекты, то отрисовываем их здесь:
 # ...
 screen.blit(background, (0, 0))  # наносим первым слоем
+screen.blit(ball, (x, y))
+# screen.blit(ball1, (x1, y1))
 pg.display.update()  # затем обновляем экран, чтобы показать изменения
 
 # главный игровой цикл:
@@ -43,10 +53,13 @@ while flag_play:
     if not flag_play:
         break
 
-    # изменение характеристик объектов:
-    # ...
+    x += 5
+    if x >= WIDTH:
+        y = 880
 
     # перерисовка экрана:
     # ...
     screen.blit(background, (0, 0))  # наносим первым слоем
+    screen.blit(ball, (x, y))
+    # screen.blit(ball1, (x1, y1))
     pg.display.update()  # обновление экрана, чтобы отобразить новую перерисовку
