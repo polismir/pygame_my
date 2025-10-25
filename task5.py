374361f7a162150af2291e5a0f89cd56729a00ff
polismir <pollismirnova221211@gmail.com>
2025-10-25 19:02:50 +0400

    Update task5.py

--- task5.py
+++ task5.py
@@ -26,17 +26,23 @@ ball = pg.Surface((BALL_WIDTH, BALL_HEIGHT), pg.SRCALPHA)
 ball.fill((0, 0, 0, 0))
 pg.draw.circle(ball, (*BLUE, 100), (BALL_WIDTH / 2, BALL_HEIGHT / 2), 40)
 x, y = 0, 135
+speed = 5
+flag = "вправо"
 
-# ball1 = pg.Surface((BALL_WIDTH, BALL_HEIGHT), pg.SRCALPHA)
-# ball1.fill((0, 0, 0, 0))
-# pg.draw.circle(ball1, (*RED, 100), (BALL_WIDTH / 2, BALL_HEIGHT / 2), 40)
-# x1, y1 = 880, 335
+
+ball1 = pg.Surface((BALL_WIDTH, BALL_HEIGHT), pg.SRCALPHA)
+ball1.fill((0, 0, 0, 0))
+pg.draw.circle(ball1, (*RED, 100), (BALL_WIDTH / 2, BALL_HEIGHT / 2), 40)
+x1, y1 = 880, 335
+speed1 = 7
+flag1 = "влево"
+
 
 # если надо до игрового цикла (=на самом старте игры) отобразить объекты, то отрисовываем их здесь:
 # ...
 screen.blit(background, (0, 0))  # наносим первым слоем
 screen.blit(ball, (x, y))
-# screen.blit(ball1, (x1, y1))
+screen.blit(ball1, (x1, y1))
 pg.display.update()  # затем обновляем экран, чтобы показать изменения
 
 # главный игровой цикл:
@@ -53,13 +59,33 @@ while flag_play:
     if not flag_play:
         break
 
-    x += 5
     if x >= WIDTH:
-        y = 880
+        flag = "влево"
+    if x <= -120:
+        flag = "вправо"
+
+    if flag == "вправо":
+        y = 135
+        x += speed
+    if flag == "влево":
+        y = 335
+        x -= speed
+
+    if x1 >= WIDTH:
+        flag1 = "влево"
+    if x1 <= -120:
+        flag1 = "вправо"
+
+    if flag1 == "вправо":
+        y1 = 135
+        x1 += speed1
+    if flag1 == "влево":
+        y1 = 335
+        x1 -= speed1
 
     # перерисовка экрана:
     # ...
     screen.blit(background, (0, 0))  # наносим первым слоем
     screen.blit(ball, (x, y))
-    # screen.blit(ball1, (x1, y1))
+    screen.blit(ball1, (x1, y1))
     pg.display.update()  # обновление экрана, чтобы отобразить новую перерисовку
