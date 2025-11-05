0451f55a72eb2a63e2d35156391539fe1c92599c
polismir <pollismirnova221211@gmail.com>
2025-11-05 19:41:56 +0400

    Update task7.py

--- task7.py
+++ task7.py
@@ -19,7 +19,24 @@ class Ball:
         self.speed = random.randint(0, 25)
         number1, number2, number3 = random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)
         self.random_color = (number1, number2, number3)
-        self.random_size = random.randint()
+        self.random_size = random.randint(5, 55)
+
+        self.ball_surf = pg.Surface((Ball.SIZE, Ball.SIZE), pg.SRCALPHA)
+        self.ball_rect = self.ball_surf.get_rect(centerx=WIDTH * 1 / 8)
+        self.ball_rect.top = HEIGHT * 1 / 4
+        self.ball_surf.fill((0, 0, 0, 0))
+        pg.draw.circle(self.ball_surf, (*self.random_color, 100), (self.ball_rect.width / 2, self.ball_rect.height / 2 - 15), self.random_size)
+
+    def draw(self, screen):  # отрисовка снаряда на экране слоем self.surf
+        screen.blit(self.ball_surf, self.ball_rect)
+
+    def move_left(self):
+        if self.ball_rect.left > self.speed:
+            self.ball_rect.left -= self.speed
+
+    def move_right(self):
+        if self.ball_rect.right < BALL_WIDTH - self.speed:
+            self.ball_rect.right += self.speed
 
 # здесь происходит инициализация:
 pg.init()
@@ -33,30 +50,11 @@ background.fill(BROWN)
 pg.draw.rect(background, GREY, (0, 150, WIDTH, 120))
 pg.draw.rect(background, GREY, (0, 350, WIDTH, 120))
 
-ball = pg.Surface((BALL_WIDTH, BALL_HEIGHT), pg.SRCALPHA)
-ball_rect = ball.get_rect(centerx=WIDTH * 1 / 8)
-ball_rect.top = HEIGHT * 1 / 4
-ball.fill((0, 0, 0, 0))
-pg.draw.circle(ball, (*BLUE, 100),(ball_rect.width / 2, ball_rect.height / 2 - 10), 40)
-speed = 5
-flag = "вправо"
+my_ball = Ball()
+screen.blit(background, (0, 0))
+my_ball.draw(screen)
+pg.display.update()
 
-
-ball1 = pg.Surface((BALL_WIDTH, BALL_HEIGHT), pg.SRCALPHA)
-ball1_rect = ball.get_rect(centerx=WIDTH * 1 / 8)
-ball1_rect.top = HEIGHT * 1 / 4
-ball1.fill((0, 0, 0, 0))
-pg.draw.circle(ball1, (*RED, 100),(ball1_rect.width / 2, ball1_rect.height / 2 - 10), 40)
-speed1 = 7
-flag1 = "вправо"
-
-
-# если надо до игрового цикла (=на самом старте игры) отобразить объекты, то отрисовываем их здесь:
-# ...
-screen.blit(background, (0, 0))  # наносим первым слоем
-screen.blit(ball, ball_rect)
-screen.blit(ball1, ball1_rect)
-pg.display.update()  # затем обновляем экран, чтобы показать изменения
 
 # главный игровой цикл:
 flag_play = True
@@ -72,33 +70,33 @@ while flag_play:
     if not flag_play:
         break
 
-    if ball_rect.left >= WIDTH:
-        flag = "влево"
-        ball_rect.top = HEIGHT * 1 / 1.75
-    if ball_rect.left <= -120:
-        flag = "вправо"
-        ball_rect.top = HEIGHT * 1 / 4
-
-    if flag == "вправо":
-        ball_rect.right += speed
-    if flag == "влево":
-        ball_rect.left -= speed
-
-    if ball1_rect.left >= WIDTH:
-        flag1 = "влево"
-        ball1_rect.top = HEIGHT * 1 / 1.75
-    if ball1_rect.left <= -120:
-        flag1 = "вправо"
-        ball1_rect.top = HEIGHT * 1 / 4
-
-    if flag1 == "вправо":
-        ball1_rect.right += speed1
-    if flag1 == "влево":
-        ball1_rect.left -= speed1
-
-    # перерисовка экрана:
-    # ...
-    screen.blit(background, (0, 0))  # наносим первым слоем
-    screen.blit(ball, ball_rect)
-    screen.blit(ball1, ball1_rect)
-    pg.display.update()  # обновление экрана, чтобы отобразить новую перерисовку
+    # if ball_rect.left >= WIDTH:
+    #     flag = "влево"
+    #     ball_rect.top = HEIGHT * 1 / 1.75
+    # if ball_rect.left <= -120:
+    #     flag = "вправо"
+    #     ball_rect.top = HEIGHT * 1 / 4
+    #
+    # if flag == "вправо":
+    #     ball_rect.right += speed
+    # if flag == "влево":
+    #     ball_rect.left -= speed
+    #
+    # if ball1_rect.left >= WIDTH:
+    #     flag1 = "влево"
+    #     ball1_rect.top = HEIGHT * 1 / 1.75
+    # if ball1_rect.left <= -120:
+    #     flag1 = "вправо"
+    #     ball1_rect.top = HEIGHT * 1 / 4
+    #
+    # if flag1 == "вправо":
+    #     ball1_rect.right += speed1
+    # if flag1 == "влево":
+    #     ball1_rect.left -= speed1
+    #
+    # # перерисовка экрана:
+    # # ...
+    # screen.blit(background, (0, 0))  # наносим первым слоем
+    # screen.blit(ball, ball_rect)
+    # screen.blit(ball1, ball1_rect)
+    # pg.display.update()  # обновление экрана, чтобы отобразить новую перерисовку
